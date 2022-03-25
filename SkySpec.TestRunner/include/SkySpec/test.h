81cf0320562d98c35ec27cb0e76e7d4e7af272b7
Mrowr Purr <mrowr.purr@gmail.com>
2022-03-24 19:40:46 -0700

    Add SkySpec.TestRunner

---
+++ SkySpec.TestRunner/include/SkySpec/test.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+
+namespace SkySpec {
+    namespace Runner {
+        void Test() {
+            std::cout << "Hi from the included test file";
+        }
+    }
+}
---
+++ SkySpec.TestRunner/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+#include "SkySpec/test.h"
+
+int main(int argc, char* argv[]) {
+    std::cout << "Hello, you are running the SkySpec program.";
+}
